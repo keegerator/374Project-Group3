98fb344ed81948bd38366d6e42693ae80bf79cb0
Keegan Dwyer <60892218+keegerator@users.noreply.github.com>
2021-10-12 16:08:12 -0400

    Popup Changes

--- app/src/main/java/com/example/a374project/MainActivity.java
+++ app/src/main/java/com/example/a374project/MainActivity.java
@@ -1,7 +1,19 @@
 package com.example.a374project;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
 
+import com.example.a374project.ui.Popup_Page_Activity;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,6 +27,8 @@ import com.example.a374project.databinding.ActivityMainBinding;
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
+
+    Button b1  ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +46,44 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
+
+
+        b1 = findViewById(R.id.sign_in);
+        b1.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent i = new Intent(MainActivity.this, Popup_Page_Activity.class);
+                        startActivity(i);
+                    }
+                }
+        );
     }
 
+//    public void onButtonShowPopupWindowClick(View view) {
+//
+//        // inflate the layout of the popup window
+//        LayoutInflater inflater = (LayoutInflater)
+//                getSystemService(LAYOUT_INFLATER_SERVICE);
+//        View popupView = inflater.inflate(R.layout.popup_window, null);
+//
+//        // create the popup window
+//        int width = LinearLayout.LayoutParams.WRAP_CONTENT;
+//        int height = LinearLayout.LayoutParams.WRAP_CONTENT;
+//        boolean focusable = true; // lets taps outside the popup also dismiss it
+//        final PopupWindow popupWindow = new PopupWindow(popupView, width, height, focusable);
+//
+//        // show the popup window
+//        // which view you pass in doesn't matter, it is only used for the window tolken
+//        popupWindow.showAtLocation(view, Gravity.CENTER, 0, 0);
+//
+//        // dismiss the popup window when touched
+//        popupView.setOnTouchListener(new View.OnTouchListener() {
+//            @Override
+//            public boolean onTouch(View v, MotionEvent event) {
+//                popupWindow.dismiss();
+//                return true;
+//            }
+//        });
+//    }
 }
---
+++ app/src/main/java/com/example/a374project/ui/Popup_Page_Activity.java
@@ -0,0 +1,4 @@
+package com.example.a374project.ui;
+
+public class Popup_Page_Activity {
+}
